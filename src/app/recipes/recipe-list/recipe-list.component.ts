6012dd5e6c7805393827d21ef1f1c587a8956b77
petromath77 <petromath77@gmail.com>
2017-12-13 23:49:42 +0300

    course-app

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply a test', 'http://edu-town.com/wp-content/uploads/2017/08/reading-a-recipe-worksheet.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
