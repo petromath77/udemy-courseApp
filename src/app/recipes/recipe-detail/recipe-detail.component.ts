e57187218ba2563c75131e3f20f57427a2402ea8
petromath77 <petromath77@gmail.com>
2018-01-07 19:12:31 +0300

    app_routing_end

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -24,4 +24,7 @@ export class RecipeDetailComponent implements OnInit {
   onAddToShoppingList(){
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
+  onEditRecipe(){
+    this.router.navigate(['edit'], {relativeTo: this.route});
+  }
 }
