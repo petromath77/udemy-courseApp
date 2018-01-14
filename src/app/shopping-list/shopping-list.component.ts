23c3a2a1feb7d79f90230f6c978091b01be9ef15
petromath77 <petromath77@gmail.com>
2018-01-14 21:54:59 +0300

    app_form1

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -26,4 +26,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ngOnDestroy(){
     this.subscription.unsubscribe();
   }
+  onEditItem(index: number){
+    this.shoppingListService.startedEditing.next(index);
+  }
 }
